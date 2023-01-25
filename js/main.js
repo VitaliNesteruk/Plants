caf2da30a27ca8371b56d996d30eb004f420cdba
Vitali Nesteruk <agent708@gmail.com>
2023-01-25 18:47:54 +0300

    Create accordion (second day)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* Cards select script */
 const select = document.querySelector('#select');
 const addresses = document.querySelectorAll('.output-information');
 
@@ -12,3 +13,32 @@ function handleSelect(e) {
 
 select.addEventListener('change', handleSelect)
 
+/* Accordion script */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+/*function accordionSlide() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+    } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+}
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", accordionSlide);
+}*/
